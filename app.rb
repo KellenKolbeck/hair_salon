84d6d83c0e89e3d5fd9a41b2645568e5214c19f3
Kellen <kellengkolbeck@gmail.com>
2015-08-27 23:23:55 -0700

    Add working paths for new stylist and stylist list pages, along with the working code for those pages

--- app.rb
+++ app.rb
@@ -8,23 +8,53 @@ require("pg")
 DB = PG.connect({:dbname => "hair_salon_test"})
 
 get('/') do
-  @name = Stylist.all()
   erb(:index)
 end
 
 get("/new_stylist") do
-  @name = Stylist.all()
-  erb(:index)
+  erb(:new_stylist)
 end
 
-post("/new_stylist") do
+post("/stylist_list") do
   name = params.fetch('name')
-  @name = Stylist.all()
   stylist = Stylist.new({:name => name, :id => nil})
   stylist.save()
-  redirect("/new_stylist")
+  redirect("/stylist_list")
 end
 
-# get("/stylist_client_list") do
-#
+get("/stylist_list") do
+  @stylists = Stylist.all()
+  erb(:stylist_list)
+end
+
+get("/stylist/:id") do
+  @new_stylist = Stylist.find(params.fetch('id').to_i)
+  erb(:stylist_page)
+end
+
+post("/clients") do
+  title = params.fetch("title")
+  stylist_id = params.fetch("stylist_id").to_i()
+  @stylist = Stylist.find(stylist_id)
+  @client = Client.new({:name => name, :stylist_id => stylist_id})
+  @client.save()
+  redirect("/stylist/:id")
+end
+
+# get("/stylists/:id/edit") do
+#   @stylist = Stylist.find(params.fetch("id").to_i())
+#   erb(:stylist_edit)
 # end
+
+patch("/stylists/:id") do
+  name = params.fetch("name")
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.update({:name => name})
+  erb(:stylist)
+end
+
+delete("/stylists/:id") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.delete()
+  @stylists = Stylist.all()
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,20 +4,10 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions,false)
 
 describe("the list path", {:type => :feature}) do
-  it("will properly load the index page showing all stylists") do
+  it("will properly load the index page ") do
     visit("/")
-    expect(page).to have_content("Our stylists:")
+    expect(page).to have_content("Bob and Tom")
   end
 end
 
-describe("post a stylist list to the index page", {:type => :feature}) do
-  it("will add a form that allows a user to post to a list of stylists") do
-    stylist = Stylist.new(:name => "Penny Thompson", :id => nil)
-    stylist.save()
-    visit("/")
-    fill_in("name", :with => "Penny Thompson")
-    click_button("Add stylist!")
-    visit("/new_stylist")
-    expect(page).to have_content("Penny Thompson")
-  end
-end
+# describe("post a stylist list to the index page", {:type => :feature}) do
