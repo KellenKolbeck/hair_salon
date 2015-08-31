4e4515f6a4d995fc7fe54c10f5660e83edbc9927
Kellen <kellengkolbeck@gmail.com>
2015-08-30 22:30:36 -0700

    Rename files to fit conventions

--- app.rb
+++ app.rb
@@ -26,35 +26,3 @@ get("/stylist_list") do
   @stylists = Stylist.all()
   erb(:stylist_list)
 end
-
-get("/stylist/:id") do
-  @new_stylist = Stylist.find(params.fetch('id').to_i)
-  erb(:stylist_page)
-end
-
-post("/clients") do
-  title = params.fetch("title")
-  stylist_id = params.fetch("stylist_id").to_i()
-  @stylist = Stylist.find(stylist_id)
-  @client = Client.new({:name => name, :stylist_id => stylist_id})
-  @client.save()
-  redirect("/stylist/:id")
-end
-
-# get("/stylists/:id/edit") do
-#   @stylist = Stylist.find(params.fetch("id").to_i())
-#   erb(:stylist_edit)
-# end
-
-patch("/stylists/:id") do
-  name = params.fetch("name")
-  @stylist = Stylist.find(params.fetch("id").to_i())
-  @stylist.update({:name => name})
-  erb(:stylist)
-end
-
-delete("/stylists/:id") do
-  @stylist = Stylist.find(params.fetch("id").to_i())
-  @stylist.delete()
-  @stylists = Stylist.all()
-end
